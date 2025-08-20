d2a1fb4f3e3da522ce7dd3e35e66e24968672b2d
RobsonRibeiro <louva.ba@gmail.com>
2025-08-20 20:02:09 -0300

    Atualização dashboard

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,7 @@ from datetime import datetime, date
 import json
 import base64
 
-# Importa o nosso gerenciador do Firebase
+
 from firebase_manager import FirebaseManager
 
 # --- Configuração da Página ---
